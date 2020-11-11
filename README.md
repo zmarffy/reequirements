26fbcc1a3ddae295ac2a0aa618284f91f1d8d818
Zeke Marffy <zmarffy@me.com>
2020-11-11 05:28:51 +0000

    update docs; add new more and less specific exceptins and requirements

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # `reequirements`
 
-`reequirements` is a useful library for defining and checking for the status of requirements needed for your Python project.
+`reequirements` is a useful library for defining and checking for the status of (not necessarily Python) requirements needed for your Python project.
+
+## Usage
+
+Create some `Requirement` objects. Specify a friendly name for the requirement, a command to be able to check that requirement, and if it should issue a warning or halt the program if it is missing.
+
+```python
+from reequirements import Requirement
+
+REQUIREMENTS = [
+    Requirement("docker", ["docker", "-v"], warn=False)
+    Requirement("fortune", ["fortune", "-v"], warn=True)
+]
+```
+
+Now, iterate through these requirements and check them.
+
+```python
+for r in REQUIREMENTS:
+    r.check()
+```
+
+`RequirementMissing` will be raised if `docker -v` exits with exit code 127 (command not found), or the more generic `RequirementError` will be raised if it exits with any other nonzero exit code.
+
+`RequirementMissingWarning` will be warned if `fortune -v` exits with exit code 127 (command not found), or the more generic `RequirementErrorWarning` will be warned if it exits with any other nonzero exit code.
+
+## Tips
+
+- Make sure your commands to check requirements end with the correct exit code! This often means using flags like `-v`, `-h` when it comes to command line tools
+
+- You do not need to check for requirements right away; you can create `Requirement` at any time and check it whenever you want
--- reequirements/api.py
+++ reequirements/api.py
@@ -1,39 +1,65 @@
 import subprocess
 import warnings
 
-ERROR_MSG = "Requirement {} not fulfilled or configured correctly; command {} returned a non-zero exit code, {}, with output:\n\n{}"
+BASE_ERROR_MSG = "Requirement {} error; command {} returned a non-zero exit code, {}, with output:\n\n{}"
+MISSING_ERROR_MSG = "Requirement {} not fulfilled; command {} not found"
 
 
-class RequirementMissing(Exception):
+class RequirementError(Exception):
 
     def __init__(self, requirement, output, exit_code):
         self.requirement = requirement
         self.output = output
         self.exit_code = exit_code
-        super().__init__(ERROR_MSG.format(self.requirement.name,
-                                          self.requirement.command, exit_code, output))
+        super().__init__(self._generate_message())
+
+    def _generate_message(self):
+        return BASE_ERROR_MSG.format(self.requirement.name, self.requirement.command, self.exit_code, self.output)
+
+
+class RequirementMissing(RequirementError):
+
+    def __init__(self, requirement):
+        self.requirement = requirement
+        self.output = ""
+        self.exit_code = 127
+        super().__init__(self.requirement, self.output, self.exit_code)
+
+    def _generate_message(self):
+        return MISSING_ERROR_MSG.format(self.requirement.name, self.requirement.command)
 
 
 class Requirement():
 
-    def __init__(self, name, command, warning=False):
+    def __init__(self, name, command, warn=False):
         self.name = name
         self.command = command
-        self.warning = warning
+        self.warn = warn
 
     def check(self):
         try:
             subprocess.check_output(self.command, stderr=subprocess.STDOUT)
             return True
+        except FileNotFoundError:
+            if not self.warn:
+                raise RequirementMissing(self) from None
+            else:
+                warnings.warn(MISSING_ERROR_MSG.format(
+                    self.name, self.command), RequirementMissingWarning, stacklevel=2)
+                return False
         except subprocess.CalledProcessError as e:
             output = e.output.decode().strip()
-            if not self.warning:
-                raise RequirementMissing(self, output, e.returncode) from None
+            if not self.warn:
+                raise RequirementError(self, output, e.returncode) from None
             else:
-                warnings.warn(ERROR_MSG.format(
+                warnings.warn(BASE_ERROR_MSG.format(
                     self.name, self.command, e.returncode, output), RequirementWarning, stacklevel=2)
-                return False
+            return False
 
 
 class RequirementWarning(Warning):
+    """A warning for when certain requirements are not met for one reason or another"""
+
+
+class RequirementMissingWarning(RequirementWarning):
     """A warning for when certain requirements are missing"""
