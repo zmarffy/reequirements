d44a97c0551875c7cfd9e9f7e5711357acaabc58
Zeke Marffy <zmarffy@me.com>
2021-03-05 02:48:01 +0000

    remove old `zetuptools` stuff

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
+import os
 import re
-from os.path import join as join_path
 
 import setuptools
 
-with open(join_path("reequirements", "__init__.py"), encoding="utf8") as f:
+with open(os.path.join("reequirements", "__init__.py"), encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 setuptools.setup(
-    name='reequirements',
+    name="reequirements",
     version=version,
-    author='Zeke Marffy',
-    author_email='zmarffy@yahoo.com',
+    author="Zeke Marffy",
+    author_email="zmarffy@yahoo.com",
     packages=setuptools.find_packages(),
     url='https://github.com/zmarffy/reequirements',
     license='MIT',
-    description='library for defining and checking for the status of requirements needed for your Python project',
-    python_requires='>=3.3',
+    description='Library for defining and checking for the status of requirements needed for your Python project',
+    python_requires='>=3.6',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    install_requires=[
-    ],
 )
